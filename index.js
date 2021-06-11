9512237ff9b7481391f47475eebf211e5e99f43e
nithipramesti <nithipramesti1508@gmail.com>
2021-06-11 13:20:18 +0800

    class

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-let obj = {
-  name: "Maria",
-  age: 28,
-  languages: ["english", "france", "korean"],
-};
+// let obj = {
+//   name: "Maria",
+//   age: 28,
+//   languages: ["english", "france", "korean"],
+// };
 
-console.log(obj.languages);
+// console.log(obj.languages);
+
+// class Student {
+//   constructor(name, birthYear) {
+//     (this.name = name),
+//       (this.age = 2021 - birthYear),
+//       (this.school = "Purwadhika"),
+//       (this.courses = ["English", "Mathematics", "History", "Music"]);
+//   }
+
+//   intro() {
+//     console.log(
+//       `My name is ${this.name}, I'm ${
+//         this.age
+//       } years old. I study ${this.courses.join(", ")} at ${this.school}`
+//     );
+//   }
+// }
+
+// let Maria = new Student("Maria Lauren", 2001);
+// Maria.intro();
+
+class Student {
+  constructor(name, birthYear, address) {
+    this.name = name;
+    this.age = 2021 - birthYear;
+    this.address = address;
+  }
+
+  intro() {
+    console.log(
+      `Hi, my name is ${this.name}. I'm ${this.age} years old, and I'm from ${this.address}.`
+    );
+  }
+}
+
+class CollegeStudent extends Student {
+  constructor(name, birthYear, address, university, scholarship) {
+    super(name, birthYear, address);
+    this.university = university;
+    this.scholarship = scholarship;
+  }
+
+  ScholarshipInfo() {
+    console.log(
+      `I get scholarship from ${this.scholarship} to study at ${this.university}`
+    );
+  }
+}
+
+let Matthew = new CollegeStudent(
+  "Matthew Brian",
+  "2001",
+  "Otario",
+  "University of Melbourne",
+  "MIFRS"
+);
+
+Matthew.intro();
+Matthew.ScholarshipInfo();
