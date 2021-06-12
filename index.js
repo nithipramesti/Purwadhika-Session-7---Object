bc9f71c53b9909e9079385739da37e19e0bfbd32
nithipramesti <nithipramesti1508@gmail.com>
2021-06-12 23:33:49 +0800

    taking notes

--- index.js
+++ index.js
@@ -26,41 +26,55 @@
 // let Maria = new Student("Maria Lauren", 2001);
 // Maria.intro();
 
-class Student {
-  constructor(name, birthYear, address) {
-    this.name = name;
-    this.age = 2021 - birthYear;
-    this.address = address;
-  }
+// class Student {
+//   constructor(name, birthYear, address) {
+//     this.name = name;
+//     this.age = 2021 - birthYear;
+//     this.address = address;
+//   }
 
-  intro() {
-    console.log(
-      `Hi, my name is ${this.name}. I'm ${this.age} years old, and I'm from ${this.address}.`
-    );
-  }
-}
+//   intro() {
+//     console.log(
+//       `Hi, my name is ${this.name}. I'm ${this.age} years old, and I'm from ${this.address}.`
+//     );
+//   }
+// }
 
-class CollegeStudent extends Student {
-  constructor(name, birthYear, address, university, scholarship) {
-    super(name, birthYear, address);
-    this.university = university;
-    this.scholarship = scholarship;
-  }
+// class CollegeStudent extends Student {
+//   constructor(name, birthYear, address, university, scholarship) {
+//     super(name, birthYear, address);
+//     this.university = university;
+//     this.scholarship = scholarship;
+//   }
 
-  ScholarshipInfo() {
-    console.log(
-      `I get scholarship from ${this.scholarship} to study at ${this.university}`
-    );
-  }
-}
+//   ScholarshipInfo() {
+//     console.log(
+//       `I get scholarship from ${this.scholarship} to study at ${this.university}`
+//     );
+//   }
+// }
 
-let Matthew = new CollegeStudent(
-  "Matthew Brian",
-  "2001",
-  "Otario",
-  "University of Melbourne",
-  "MIFRS"
-);
+// let Matthew = new CollegeStudent(
+//   "Matthew Brian",
+//   "2001",
+//   "Otario",
+//   "University of Melbourne",
+//   "MIFRS"
+// );
 
-Matthew.intro();
-Matthew.ScholarshipInfo();
+// Matthew.intro();
+// Matthew.ScholarshipInfo();
+
+// //EXERCISE 1
+// let dateNow = new Date();
+// console.log(dateNow.getMonth());
+
+let student = {
+  name: "Sabastian String",
+  age: 20,
+  class: ["Art", "Mathematics", "Science"],
+  pass: true,
+  intro: function (name, age) {
+    console.log(`Hi, my name is ${this.name}. I'm ${this.age} years old.`);
+  },
+};
